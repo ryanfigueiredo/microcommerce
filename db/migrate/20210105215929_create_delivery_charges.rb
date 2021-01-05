ddcdd90d27020a77a6dc221c460d64a92827158a
ryanfigueiredo <ryanfigueiredo@hotmail.com>
2021-01-05 20:36:26 -0300

    correction of migrate delivery change field county and price

--- db/migrate/20210105215929_create_delivery_charges.rb
+++ db/migrate/20210105215929_create_delivery_charges.rb
@@ -1,8 +1,8 @@
 class CreateDeliveryCharges < ActiveRecord::Migration[6.0]
   def change
     create_table :delivery_charges do |t|
-      t.string :county, precision: 10, scale: 2, null: false
-      t.decimal :price, null: false
+      t.string :county, null: false
+      t.decimal :price, precision: 10, scale: 2, null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -44,7 +44,7 @@ ActiveRecord::Schema.define(version: 2021_01_05_215929) do
 
   create_table "delivery_charges", force: :cascade do |t|
     t.string "county", null: false
-    t.decimal "price", null: false
+    t.decimal "price", precision: 10, scale: 2, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
