a6bfc8b25a6d23d01867c4dbeb66916fe5ed9efa
ryanfigueiredo <ryanfigueiredo@hotmail.com>
2021-01-12 10:56:03 -0300

    adding the index action to the customerOrder controller and improvements to the customer_orders form

--- app/controllers/admin/customer_orders_controller.rb
+++ app/controllers/admin/customer_orders_controller.rb
@@ -1,10 +1,14 @@
 class Admin::CustomerOrdersController < ApplicationController
+  def index
+    @customer_orders = CustomerOrder.order(:created_at)
+  end
+
   def create
   end
 
   private
 
   def set_customer_order_params
-    params.require(:customer_orders).permit(:total_value, :delivery_address, :delivery_charge_id, :way_of_payment, :observation, :change_for)
+    params.require(:customer_order).permit(:total_value, :delivery_address, :delivery_charge_id, :way_of_payment, :observation, :change_for)
   end
 end
--- app/javascript/packs/order/index.js
+++ app/javascript/packs/order/index.js
@@ -1,2 +1,16 @@
 require('./controllers/add_item_bag')
 require('./controllers/remove_item_bag')
+
+$(document).on('turbolinks:load', function() {
+  displayChangeFor()
+})
+
+var displayChangeFor = () => {
+  $('#customer_order_way_of_payment').change(function() {
+    if($('#customer_order_way_of_payment option:selected').val() == 'money') {
+      $('#form-group-money').show()
+    } else {
+      $('#form-group-money').hide()
+    }
+  })
+}
