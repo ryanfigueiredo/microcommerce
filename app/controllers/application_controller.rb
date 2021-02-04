7d53be42ec173abe1ca8a20b9f4d2498d63151b0
Cesar Ryan <ryanfigueiredo@hotmail.com>
2021-02-03 22:11:29 -0300

    add best practice in current_setting, refactoring set_setting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
-  before_action :set_setting
+  helper_method :current_setting
 
   def format_price(price)
     format("%.2f", price) if price.present?
   end
 
-  def set_setting
-    @setting = Setting.first
+  def current_setting
+    Setting.first
   end
 end
