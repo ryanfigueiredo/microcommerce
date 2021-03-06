fb7351778c761cf59d96822c187b7c05936e8345
Cesar Ryan <ryanfigueiredo@hotmail.com>
2021-03-06 18:49:03 -0300

    change project name

--- db/seeds/20210205191958_create_initial_setting.rb
+++ db/seeds/20210205191958_create_initial_setting.rb
@@ -1,7 +1,7 @@
 puts "creating initial setting..."
   setting = Setting.create!
   setting.logo.attach(
-    io: File.open("/home/ryanfigueiredo/Documentos/dev/hortifrutidakelly/app/assets/images/logo.jpg"),
+    io: File.open("/home/ryanfigueiredo/Documentos/dev/microcommerce/app/assets/images/logo.jpg"),
     filename: 'logo.jpg',
     content_type: 'image/jpg',
     identify: false
